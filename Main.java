10006688be3a35a7f0eed108d6537b995e4c9b99
Ethan Katz <ethan.katz@mynbps.org>
2020-12-15 16:32:00 +0000

    started my project with user input

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("What is your first name? Up to 5 letters.");
+        String fName = scanner.nextLine();
+        System.out.println("What is your last name? Up to 5 letters");
+        String lName = scanner.nextLine();
+
+
+    }
+}
